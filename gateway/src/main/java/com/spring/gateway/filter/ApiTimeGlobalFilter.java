0d5818ac4a27287e95097e411f1c08549fc2ee79
1234www4321 <74413921+1234www4321@users.noreply.github.com>
2025-12-03 13:44:58 +0800

    update

--- gateway/src/main/java/com/spring/gateway/filter/ApiTimeGlobalFilter.java
+++ gateway/src/main/java/com/spring/gateway/filter/ApiTimeGlobalFilter.java
@@ -1,8 +1,11 @@
 package com.spring.gateway.filter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -13,11 +16,26 @@ import reactor.core.publisher.Mono;
  * @description
  * @date 2025/12/2 10:12
  */
+@Component
+
 public class ApiTimeGlobalFilter implements GlobalFilter, Ordered {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    public static final String BEGIN_TIME="begin_time";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
+        exchange.getAttributes().put(BEGIN_TIME,System.currentTimeMillis());
+        return chain.filter(exchange).then(Mono.fromRunnable(()->{
+            Long beginTime = exchange.getAttribute(BEGIN_TIME);
+            if(beginTime != null){
+                logger.info("访问接口主机:{}",exchange.getRequest().getURI().getHost()+":"+exchange.getRequest().getURI().getPort());
+                logger.info("访问接口url:{}",exchange.getRequest().getURI().getPath());
+                logger.info("访问接口时长:{}",System.currentTimeMillis()-beginTime);
+                logger.info("=======================api===================");
+            }
+        }));
     }
 
     @Override
--- gateway/src/main/java/com/spring/gateway/filter/AuthenticationGlobalFilter.java
+++ gateway/src/main/java/com/spring/gateway/filter/AuthenticationGlobalFilter.java
@@ -5,11 +5,13 @@ import com.spring.gateway.webclient.AuthWebClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.stream.Collectors;
 
 /**
@@ -33,12 +35,23 @@ public class AuthenticationGlobalFilter implements GlobalFilter {
                         .header("X-User-Id", String.valueOf(authResponse.getUserId()))
                         .header("X-Roles", authResponse.getRoleCode().stream().map(String::valueOf).collect(Collectors.joining(",")))
                         .build();
-
+                return chain.filter(exchange.mutate().request(mutatedRequest).build());
+            }else{
+                return unauthorized(exchange);
             }
-        })
+        }).timeout(Duration.ofSeconds(5),Mono.defer(()->{
+                exchange.getResponse().setStatusCode(HttpStatus.GATEWAY_TIMEOUT);
+                return exchange.getResponse().setComplete();
+            }
+        ));
 
     }
     public void isExcluded(){
 
+    }
+
+    public Mono<Void> unauthorized(ServerWebExchange exchange){
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
     }
 }
